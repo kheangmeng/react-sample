2ab1e6a52b044bc95dc84c720f1e3c79e9b6298c
kheangmeng <kheangmengte@gmail.com>
2025-06-26 10:48:43 +0700

    update repo

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: process.env.NODE_ENV === 'production' ? '/react-sample/' : '/',
+})
